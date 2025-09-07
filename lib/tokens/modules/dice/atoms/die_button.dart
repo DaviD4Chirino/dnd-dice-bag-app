b30be96d8677688d6a8ddf395f8182e1b2dcccf1
David Space <genarodavid@gmail.com>
2025-09-07 16:07:12 -0400

    made DieExtraModifierInput

--- lib/tokens/modules/dice/atoms/die_button.dart
+++ lib/tokens/modules/dice/atoms/die_button.dart
@@ -1,6 +1,7 @@
 import 'package:dice_bag/tokens/app/app_sizing.dart';
 import 'package:dice_bag/tokens/models/enums/die_faces.dart';
 import 'package:dice_bag/tokens/modules/dice/atoms/die_amount_modifier_button.dart';
+import 'package:dice_bag/tokens/modules/dice/atoms/die_extra_modifier_input.dart';
 import 'package:dice_bag/tokens/modules/dice/atoms/polymath.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -47,21 +48,28 @@ class DieButton extends StatelessWidget {
           ),
           Positioned.fill(
             child: Align(
-              alignment: Alignment.bottomLeft,
-              child: DieAmountModifierButton(
-                increase: true,
-                onPressed: null,
-                alignment: Alignment.bottomLeft,
-              ),
-            ),
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: DieAmountModifierButton(
-                increase: false,
-                onPressed: null,
-                alignment: Alignment.bottomRight,
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: AppSizing.md,
+                child: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    DieAmountModifierButton(
+                      increase: true,
+                      onPressed: null,
+                      alignment: Alignment.bottomLeft,
+                    ),
+                    Expanded(child: DieExtraModifierInput()),
+                    DieAmountModifierButton(
+                      increase: false,
+                      onPressed: null,
+                      alignment: Alignment.bottomRight,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
---
+++ lib/tokens/modules/dice/atoms/die_extra_modifier_input.dart
@@ -0,0 +1,40 @@
+import 'package:dice_bag/tokens/app/app_spacing.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// A Scroll that lets you increase the amount by that many
+class DieExtraModifierInput extends StatefulWidget {
+  const DieExtraModifierInput({super.key});
+
+  @override
+  State<DieExtraModifierInput> createState() =>
+      _DieExtraModifierInputState();
+}
+
+class _DieExtraModifierInputState
+    extends State<DieExtraModifierInput> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    var inputDecoration = InputDecoration(
+      border: InputBorder.none,
+      alignLabelWithHint: true,
+      contentPadding: EdgeInsets.all(AppSpacing.sm),
+      isDense: true,
+      hintText: "-1  0  +1",
+      hintStyle: theme.textTheme.labelLarge?.copyWith(
+        color: theme.colorScheme.onSurface.withAlpha(50),
+      ),
+    );
+    return TextField(
+      decoration: inputDecoration,
+      inputFormatters: [
+        FilteringTextInputFormatter.allow(RegExp(r'[0-9+-]')),
+      ],
+      cursorHeight: 20,
+      style: theme.textTheme.labelLarge,
+      textAlign: TextAlign.center,
+      textAlignVertical: TextAlignVertical.center,
+    );
+  }
+}
